5da7207c82c763057eb3c4d3db83ed61937716f1
gpl.programs.info@gmail.com <gpl.programs.info@gmail.com@2789776a-bd8c-0347-f9d7-aabbff61dc36>
2010-12-08 09:05:32 +0000

    modified makeUI.bat to also package locale for xulsword

--- trunk/localeDev/makeUI.bat
+++ trunk/localeDev/makeUI.bat
@@ -1,7 +1,15 @@
-REM usage: UI-listing.pl MK MKS locale version alternateLocale sourcingFromFirefox3(true|false)
-REM The next line creates the UI-listing files in %MKS%\localeDev\es-MX
-::UI-listing.pl "%MK%" "%MKS%" fa 2.16 en-US false
+REM usage: UI-listing.pl MK MKS locale version alternateLocale sourceFromFirefox3(true|false)
+REM   Create the UI-listing text file(s)
+REM   Output goes to: %MKS%\localeDev\<locale-code>
+UI-listing.pl "%MK%" "%MKS%" ex 2.15 en-US true
 
 REM usage: UI-code.pl MK MKS locale noShortcutKeys(true|false)
-REM When the UI-listing file is translated, then run the following
-UI-code.pl "%MK%" "%MKS%" ct true
+REM   Read the UI-listing text files and create a locale
+REM   output goes to: %MKS%\localeDev\<locale-code>\locale
+UI-code.pl "%MK%" "%MKS%" ex true
+
+REM usage: UpdateJars.pl MK MKS MKO isProduction UIversion MinProgversionForUI IncludeLocales AllLocales
+REM   Package the locale for xulsword
+REM   output goes to: "%MKS%\localeDev\locales\
+"%MK%\build\script\UpdateJars.pl" "%MK%" "%MKS%" "" true 2.9 2.9 ex ex
+
