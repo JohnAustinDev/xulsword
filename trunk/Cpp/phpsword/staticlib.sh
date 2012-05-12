548d3881ab20da20adb0b8fa75df074aee451726
gpl.programs.info@gmail.com <gpl.programs.info@gmail.com@2789776a-bd8c-0347-f9d7-aabbff61dc36>
2012-05-12 11:48:55 +0000

    phpsword static released

--- trunk/Cpp/phpsword/staticlib.sh
+++ trunk/Cpp/phpsword/staticlib.sh
@@ -3,14 +3,16 @@
 rm .libs/phpswordstatic.so
 
 cc -shared \
+-pthread  \
 .libs/phpsword.o \
 .libs/.o \
--pthread  \
 ../clucene-core-0.9.21b/src/*.o \
 ../sword-svn/lib/*.o \
--lstdc++ \
--lz \
--lm \
+-static \
+/usr/lib/i386-linux-gnu/gcc/i686-linux-gnu/4.5/libstdc++.a \
+/usr/lib/i386-linux-gnu/libz.a \
+/usr/lib/i386-linux-gnu/libm.a \
+--unresolved-symbols=report-all \
 -Wl,-soname -Wl,phpswordstatic.so -o .libs/phpswordstatic.so
 
 cp .libs/phpswordstatic.so /usr/lib/php5/20090626+lfs/phpsword.so
