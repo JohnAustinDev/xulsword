7ac46e1025695bc78991f529a0d2df36251fb154
gpl.programs.info@gmail.com <gpl.programs.info@gmail.com@2789776a-bd8c-0347-f9d7-aabbff61dc36>
2014-04-30 02:30:02 +0000

    xulsword 3.10, libxulsword 1.1 release candidate

--- trunk/build.sh
+++ trunk/build.sh
@@ -4,6 +4,7 @@ find . -name "*.pl" -exec chmod ugo+x {} \;
 
 sudo apt-get update
 
+# [ ! -e ./Cpp ] means running in Vagrant
 if [ ! -e ./Cpp ]; then 
 	noprompt="-y"
 fi 
@@ -23,7 +24,7 @@ else
 	cd /vagrant/Cpp
 fi
 
-# Compile CLucene (using pre-existing code if it exists)
+# Compile CLucene
 if [ ! -e ./clucene-core-0.9.21b ]; then
 	wget http://sourceforge.net/projects/clucene/files/clucene-core-stable/0.9.21b/clucene-core-0.9.21b.tar.bz2/download
 	tar -xf download 
@@ -37,17 +38,51 @@ sudo make install
 sudo ldconfig
 cd ..
 
-# Compile SWORD engine (using pre-existing code if it exists)
+: <<'COMMENT'
+# Compile XZ Utils
+if [ ! -e ./xz-5.0.5 ]; then
+	wget http://tukaani.org/xz/xz-5.0.5.tar.bz2
+	tar -xf xz-5.0.5.tar.bz2 
+	rm xz-5.0.5.tar.bz2
+fi
+cd xz-5.0.5
+make clean
+./configure
+make
+sudo make install
+sudo ldconfig
+cd ..
+
+# Compile bzip2
+if [ ! -e ./clucene-core-0.9.21b ]; then
+	wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
+	tar -xf bzip2-1.0.6.tar.gz 
+	rm bzip2-1.0.6.tar.gz
+fi
+cd bzip2-1.0.6
+make clean
+make
+sudo make install
+sudo ldconfig
+cd ..
+COMMENT
+
+# Compile SWORD engine
+swordRev=3203
 if [ ! -e ./sword-svn ]; then
 	mkdir sword-svn
 	cd sword-svn
-	svn checkout -r 3008 http://crosswire.org/svn/sword/trunk ./
+	svn checkout -r $swordRev http://crosswire.org/svn/sword/trunk ./
 	cd ..
+else
+  cd sword-svn
+  svn update -r $swordRev
+  cd ..
 fi
 cd sword-svn
 make clean
 ./autogen.sh
-./configure --without-icu
+./configure --without-icu --without-xz --without-bzip2
 make
 sudo make install
 sudo ldconfig
