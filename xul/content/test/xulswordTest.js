67cdca16ad8bbbd3b43c01d2ae468c78174337f3
Tom Roth <tomr2k@yahoo.com>
2021-10-09 20:53:11 -0700

    removes Firefox dependency when displaying test messages.

--- xul/content/test/xulswordTest.js
+++ xul/content/test/xulswordTest.js
@@ -42,9 +42,10 @@ setCipherKey                test with xulsword
 
 function jsdump(str)
 {
-  Components.classes['@mozilla.org/consoleservice;1']
-            .getService(Components.interfaces.nsIConsoleService)
-            .logStringMessage(str);
+  console.log(str);
+  // Components.classes['@mozilla.org/consoleservice;1']
+  //           .getService(Components.interfaces.nsIConsoleService)
+  //           .logStringMessage(str);
 }
 
 var NumFailed = 0;
