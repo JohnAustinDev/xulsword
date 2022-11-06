91bb258fdae0f523c0a0791ff6d9aadf591ce474
Vagrant User <vm@vagrant.net>
2022-11-06 14:23:44 -0800

    Use fixed node version 16.14.0

--- init.sh
+++ init.sh
@@ -65,8 +65,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install --lts
-nvm use node
+nvm install 16.14.0
+nvm use 16.14.0
 npm i --global yarn
 
 # Create a local Cpp installation directory
