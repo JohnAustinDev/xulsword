b556a6694cfb14479d559c17b5196d22b58c9aad
Vagrant User <vm@vagrant.net>
2022-12-06 16:28:26 -0800

    Windows cross-compile with problems

--- libxulsword/rebuild.js
+++ libxulsword/rebuild.js
@@ -1,16 +1,28 @@
 #! /usr/bin/env node
+const { copyFileSync } = require('fs');
+const path = require('path');
 const { exec } = require('child_process');
 const nodeGyp = require("node-gyp")();
 
-// Check the environment variables correspond to the current build.
-console.log(`ARGUMENTS: ${process.argv}`);
+console.log(`Cross-compiling with ${process.env.CXX_target}`);
+
+process.chdir(__dirname);
+
+if (!process.env.TOOLCHAIN_PREFIX)
+  throw new Error(`Environment has not be set. Run 'source ./setenv'`);
+/*
+if (process.env.XCOMP && (
+    !process.env.TOOLCHAIN_PREFIX.endsWith(process.env.XCOMP) ||
+    process.env.ADDRESS_MODEL !== process.env.XADS
+)) {
+  throw new Error(`${process.env.CXX_target} does not match environment vars XCOMP and/or XADS`);
+}
+*/
 console.log(`TOOLCHAIN: ${process.env.TOOLCHAIN_PREFIX}`);
 console.log(`ADDRESS_MODEL: ${process.env.ADDRESS_MODEL}`);
 console.log(`GCCSTD: ${process.env.GCCSTD}`);
 
 nodeGyp.parseArgv([]);
-
-console.log(`Cross-compiling to windows with ${process.env.CXX_target }`);
 
 new Promise((resolve) => {
   nodeGyp.commands.clean([], () => resolve());
@@ -35,5 +47,14 @@ new Promise((resolve) => {
   return new Promise((resolve) => {
     nodeGyp.commands.build([], () => resolve());
   });
+})
+.then(() => {
+  // The cross compiled lib always goes to Release. So if this is
+  // a host build, copy the correct lib over.
+  if (!process.env.XCOMP) {
+    const destdir = path.join(__dirname, 'build', 'Release');
+    const src = path.join(destdir, `xulswordHost.node`)
+    // copyFileSync(src, path.join(destdir, 'xulsword.node'));
+  }
 })
 .catch((er) => console.log(er));
