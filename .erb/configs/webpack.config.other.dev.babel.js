2a84ab8bc130a0dae8f2be22623d5a854b4846a7
Vagrant User <vm@vagrant.net>
2024-04-25 20:48:59 -0700

    Added missing webpack config other

---
+++ .erb/configs/webpack.config.other.dev.babel.js
@@ -0,0 +1,79 @@
+import path from 'path';
+import fs from 'fs';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import chalk from 'chalk';
+import { merge } from 'webpack-merge';
+import { spawn, execSync } from 'child_process';
+import baseConfig, { xulswordEntries, rulesDev } from './webpack.config.base';
+import webpackPaths from './webpack.paths.js';
+import checkNodeEnv from '../scripts/check-node-env';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+
+const entries = {};
+xulswordEntries.forEach((pth) => {
+  const name = path.basename(pth);
+  entries[name] = {
+    import: [
+      pth
+    ]
+  };
+});
+
+// When an ESLint server is running, we can't set the NODE_ENV so we'll check if it's
+// at the dev webpack config is not accidentally run in a production environment
+if (process.env.NODE_ENV === 'production') {
+  checkNodeEnv('development');
+}
+
+export default merge(baseConfig, {
+  devtool: 'source-map',
+
+  mode: 'development',
+
+  target: ['web', 'electron-renderer'],
+
+  entry: entries,
+
+  output: {
+    path: webpackPaths.distPath,
+    publicPath: '/',
+    filename: '[name].dev.js',
+    library: {
+      type: 'umd',
+    },
+  },
+
+  module: {
+    rules: rulesDev,
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+
+    /**
+     * Create global constants which can be configured at compile time.
+     *
+     * Useful for allowing different behaviour between development builds and
+     * release builds
+     *
+     * NODE_ENV should be production so that modules do not perform certain
+     * development checks
+     *
+     * By default, use 'development' as NODE_ENV. This can be overriden with
+     * 'staging', for example, by changing the ENV variables in the npm scripts
+     */
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+    }),
+
+    new webpack.LoaderOptionsPlugin({
+      debug: true,
+    }),
+
+  ],
+
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+});
