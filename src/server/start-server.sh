7bed974a390e6eaecb960b87cf7900471426e110
Vagrant User <vm@vagrant.net>
2024-06-03 21:01:15 -0700

    Do setenv in start-server.sh

--- src/server/start-server.sh
+++ src/server/start-server.sh
@@ -3,5 +3,7 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )/../../"
 XULSWORD="$(pwd)"
 
-export LD_LIBRARY_PATH=./Cpp/lib 
+source ./setenv
+
+export LD_LIBRARY_PATH=./Cpp/lib
 $XULSWORD/../.nvm/versions/node/v20.12.2/bin/node -r ./.erb/scripts/babel-register ./src/server/server.ts
