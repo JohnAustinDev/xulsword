d9d9c0cd313f3ce2a22367256b2259a04a0163fa
Vagrant User <vm@vagrant.net>
2024-06-08 22:15:06 -0700

    Improved start-server script

--- src/server/start-server.sh
+++ src/server/start-server.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/../../"
-XULSWORD="$(pwd)"
 
 source ./setenv
 
 export LD_LIBRARY_PATH=./Cpp/lib
-$XULSWORD/../.nvm/versions/node/v20.12.2/bin/node -r ./.erb/scripts/babel-register ./src/server/server.ts
+"$NVM_BIN/node" -r ./.erb/scripts/babel-register ./src/server/server.ts
