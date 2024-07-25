b5874508143e63ef22af65e42ebd515c95b52334
Vagrant User <vm@vagrant.net>
2024-07-25 13:09:01 -0700

    Updated webapp build.sh

--- src/clients/webapp/build.sh
+++ src/clients/webapp/build.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/bash
 
-OUTDIR=$1
-DEVELOPMENT=$2
+DIST_PARENT_DIR=$1
+IS_DEVELOPMENT=$2
 
-if [[ -z $OUTDIR || ! -d "$OUTDIR/dist/" ]]; then echo "ERROR: no dist '$OUTDIR/dist'." && exit 1; fi
-if [[ "$OUTDIR" =~ ^\. ]]; then OUTDIR="$(pwd)/./$OUTDIR"; fi
+if [[ -z "$DIST_PARENT_DIR" || ! -d "$DIST_PARENT_DIR/dist/" ]]; then 
+  echo "Usage: build.sh DIST_PARENT_DIR [IS_DEVELOPMENT]"
+  echo "ERROR: Not a directory: $DIST_PARENT_DIR/dist"
+  exit 1
+fi
 
-cd "$( dirname "${BASH_SOURCE[0]}" )/../../"
+if [[ "$DIST_PARENT_DIR" =~ ^\. ]]; then
+  DIST_PARENT_DIR="$(pwd)/./$DIST_PARENT_DIR"
+fi
 
-rm "$OUTDIR/dist/"*;
+cd "$( dirname "${BASH_SOURCE[0]}" )/../../.." || exit 5
+
+rm "$DIST_PARENT_DIR/dist/"*;
 rm -rf "./build/app/dist/"*;
 
 source ./setenv
 
-if [[ "$DEVELOPMENT" == "1" ]]; then
-  yarn build:other-dev
+if [[ "$IS_DEVELOPMENT" == "1" ]]; then
+  yarn webpack --env development --env webappClients
 else
-  yarn build:other
+  yarn webpack --env production --env webappClients
 fi
 
 if [[ "$?" == "0" ]]; then
-  cp "./build/app/dist/"* "$OUTDIR/dist"
-  cp "./src/browser/bibleBrowser.html" "$OUTDIR/dist"
+  cp "./build/webapp/dist/webappClients/"* "$DIST_PARENT_DIR/dist"
 else
   echo "ERROR: react build failed."
   exit 1;
