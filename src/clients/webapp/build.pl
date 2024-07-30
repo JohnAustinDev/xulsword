f53ade7120627744030b371b778120aab7b36d41
Vagrant User <vm@vagrant.net>
2024-07-29 17:36:38 -0700

    Converted build.sh to build.pl

---
+++ src/clients/webapp/build.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $DIST_PARENT_DIR = shift;
+my $IS_DEVELOPMENT = shift;
+
+if (! "$DIST_PARENT_DIR" || ! -d "$DIST_PARENT_DIR/dist/") {
+  print "Usage: build.sh DIST_PARENT_DIR [IS_DEVELOPMENT]";
+  print "ERROR: Not a directory: $DIST_PARENT_DIR/dist";
+  exit 1;
+}
+
+if ("$DIST_PARENT_DIR" =~ /^\./) {
+  $DIST_PARENT_DIR=`pwd` . "/./$DIST_PARENT_DIR";
+}
+
+{ my $path = $0; $path =~ s/\/[^\/]+$//; chdir("$path/../../.."); }
+
+`rm "$DIST_PARENT_DIR/dist/"*`;
+`rm -rf "./build/app/dist/"*`;
+
+`source ./setenv`;
+
+if ("$IS_DEVELOPMENT" eq "1") {
+  open(YARN, "yarn webpack --env development --env webappClients |");
+} else {
+  open(YARN, "yarn webpack --env production --env webappClients |");
+}
+while (<YARN>) { print $_;}
+close(YARN);
+
+if ("$?" eq "0") {
+  my $dist = "$DIST_PARENT_DIR/dist";
+  `cp "./build/webapp/dist/webappClients/"* "$dist"`;
+  my $libs = "ibt.libraries.yml";
+  chdir "$DIST_PARENT_DIR" || die;
+  if (-e "$libs") {
+    opendir(JSF, "$dist") || die;
+    my @files = readdir(JSF);
+    closedir(JSF);
+    my $file; foreach my $f (@files) {if ($f =~ /^widgets_.*\.js$/) {$file = $f; last;}}
+    open(INF, "<:encoding(UTF-8)", "$libs") || die;
+    open(OUTF, ">:encoding(UTF-8)", "$libs.tmp") || die;
+    while(<INF>) {s/(?<=\bdist\/)widgets_.*\.js(?=:)/$file/; print OUTF $_;}
+    close(INF); close(OUTF); `mv "$libs.tmp" "$libs"`;
+  }
+} else {
+  `echo "ERROR: react build failed."`;
+  exit 1;
+}
