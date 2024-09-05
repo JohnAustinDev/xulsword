fff21c759bf26b6c34341b299bdf63045087cf65
Vagrant User <vm@vagrant.net>
2024-09-05 10:12:02 -0700

    Updated start-server.sh

--- src/servers/webapp/start-server.sh
+++ src/servers/webapp/start-server.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/bash
 
-NODE_VERSION=22.2.0
-NVM_DIR=../.nvm
-
-cd "$( dirname "${BASH_SOURCE[0]}" )/../../../"
+NODE_EXE=$1
 
 # NOTE: setenv is NOT sourced because this script only runs a previously built
 # server which already has its env baked in.
 
+if [[ ! -e "$NODE_EXE" ]]; then 
+  echo 'Usage: ./start-server.sh path-to-node-executable'
+  exit
+fi
+
+cd "$( dirname "${BASH_SOURCE[0]}" )/../../../"
+
 export LD_LIBRARY_PATH='./Cpp/lib'
 
-NODE_EXE=versions/node/v$NODE_VERSION/bin/node
-
-if [[ -e "$NVM_DIR" ]];
-then
-  if [[ -e "$NVM_DIR/$NODE_EXE" ]];
-  then
-    "$NVM_DIR/$NODE_EXE" ./build/webapp
-  else
-    echo "ERROR: .nvm/$NODE_EXE does not exist. Run 'nvm install $NODE_VERSION'"
-  fi
-else
-  echo "ERROR: Wrong path to .nvm directory: $NVM_DIR"
-fi
+"$NODE_EXE" ./build/webapp
