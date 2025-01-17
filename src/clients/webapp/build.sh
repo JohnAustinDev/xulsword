d5102f91985b92d0fdcbf383b76d1bfd9fbc152f
Vagrant User <vm@vagrant.net>
2025-01-17 14:21:05 -0800

    Provide nvm path for webapp build scripts

--- src/clients/webapp/build.sh
+++ src/clients/webapp/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
-
+"$HOME/.nvm/nvm.sh" use 22
 source ./setenv
 
 ./src/clients/webapp/builder.pl $1 $2
--- src/servers/webapp/build-server.sh
+++ src/servers/webapp/build-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-cd "$( dirname "${BASH_SOURCE[0]}" )/../../../"
-nvm use 22
+cd "$(dirname "${BASH_SOURCE[0]}" )/../../../"
+"$HOME/.nvm/nvm.sh" use 22
 source ./setenv
 
 yarn webpack --env development --env webappSrv
