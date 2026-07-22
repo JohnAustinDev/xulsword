d5698fc644b6c49853e2dcc3aa964d6a398d3bfd
Vagrant User <vm@vagrant.net>
2026-07-22 08:48:03 -0700

    Fix to previous commit

--- src/servers/webapp/start-server.sh
+++ src/servers/webapp/start-server.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/bash
 
-NODE_EXE=$1
-
-# NOTE: setenv is NOT sourced because this script only runs a previously built
-# server which already has its env baked in.
-
-if [[ ! -e "$NODE_EXE" ]]; then 
-  echo 'Usage: ./start-server.sh path-to-node-executable'
-  exit
-fi
+# NOTE: setenv variables might not have an expected effect because this script
+# runs a previously built server which already has its env baked in.
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/../../../"
-
+source ./setenv
 export LD_LIBRARY_PATH='./Cpp/lib'
 
-"$NODE_EXE" --preserve-symlinks ./build/webapp
+# The --preserve-symlinks flag is required since the package.json dependency
+# of "libxulsword": "portal:./libxulsword" creates a symlink to the module.
+"$NVM_DIR/versions/node/v${NODE_VERSION}/bin/node" --preserve-symlinks ./build/webapp
