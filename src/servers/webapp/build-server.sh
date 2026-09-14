67d5d0ff576e69238182e70a26ed483feb9f1285
Vagrant User <vm@vagrant.net>
2026-09-14 15:04:44 -0700

    Build fixes

--- src/servers/webapp/build-server.sh
+++ src/servers/webapp/build-server.sh
@@ -5,7 +5,14 @@ DEVEL=$1
 cd "$(dirname "${BASH_SOURCE[0]}" )/../../../"
 source "$HOME/.nvm/nvm.sh"
 source ./setenv
+
+cd ./build/app
 yarn install
+
+cd ../..
+yarn install
+
+yarn install-libxulsword
 
 if [[ -z "$DEVEL" ]]; then
   yarn webpack --env production --env webappSrv
